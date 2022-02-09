d5ea64b972d2a2495156dea11c971615762eb1fe
bellwest <bellwest93@gmail.com>
2022-02-10 01:00:20 +0900

    Create post model and implement post functino

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,105 +1,24 @@
-import { SSL_OP_MICROSOFT_SESS_ID_BUG } from "constants";
+import Post from '../../models/post';
 
-let postId = 1;
-
-const posts = [
-  {
-    id: 1,
-    title: 'title',
-    body: 'body',
-  },
-];
-
-/*
-POST /api/posts
-{ title, body }
-*/
-export const write = ctx => {
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
-}
-
-/*
-GET /api/posts
-*/
-export const list = ctx => {
-  ctx.body = posts;
-}
-
-/*
-GET /api/posts/:id
-*/
-export const read = ctx => {
-  const { id } = ctx.params;
-  const post = posts.find(p => p.id.toString() === id);
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'post does not exist',
-    };
-    return;
+export const write = async ctx => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
-}
+};
 
-/*
-DELETE /api/posts/:id
-*/
-export const remove = ctx => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (!index) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'post does not exist',
-    };
-    return;
-  }
-  posts.splice(index, 1);
-  ctx.status = 204;
-}
+export const list = ctx => {};
 
-/*
-PUT /api/posts/:id
-{ title, body }
-*/
-export const replace = ctx => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (!index) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'post does not exist',
-    };
-    return;
-  }
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-}
+export const read = ctx => {};
 
-/*
-PATCH /api/posts/:id
-{ title, body }
-*/
-export const update = ctx => {
-  const { id } = ctx.params;
-  const index = posts.findIndex(p => p.id.toString() === id);
-  if (!index) {
-    ctx.status = 404;
-    ctx.body = {
-      message: 'post does not exist',
-    };
-    return;
-  }
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-}
+export const remove = ctx => {};
+
+export const update = ctx => {};
